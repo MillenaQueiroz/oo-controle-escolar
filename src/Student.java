f428f5608cba7b3740c863187110baf64b264ba5
Millena Queiroz <millena.queiroz08@gmail.com>
2023-01-10 21:56:07 -0300

    create user class

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,20 @@
-public class Student {
+public class Student extends User {
+    private String matricula;
+    private String filiacao;
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getFiliacao() {
+        return filiacao;
+    }
+
+    public void setFiliacao(String filiacao) {
+        this.filiacao = filiacao;
+    }
 }
---
+++ src/Teacher.java
@@ -0,0 +1,11 @@
+public class Teacher extends User {
+    private String formacao;
+
+    public String getFormacao() {
+        return formacao;
+    }
+
+    public void setFormacao(String formacao) {
+        this.formacao = formacao;
+    }
+}
